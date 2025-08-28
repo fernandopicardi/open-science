6efcb88235f7268a46f93ae72b65f518698a1632
fernandopicardi <fernandopicardi@gmail.com>
2025-08-28 04:41:30 -0300

    Remove Vercel Speed Insights

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,7 @@
-import { SpeedInsights } from "@vercel/speed-insights/next";
-
 function Home() {
-  return (
-    <div>
-      <h1>Open Science</h1>
-      <SpeedInsights />
-    </div>
-  );
+  return <div>
+    <h1>Open Science</h1>
+  </div>;
 }
 
 export default Home;
