6a303c375d99921a85fc933adbe74981da4ede92
fernandopicardi <fernandopicardi@gmail.com>
2025-08-28 04:34:03 -0300

    Add Vercel Speed Insights

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 function Home() {
-  return <div>
-    <h1>Open Science</h1>
-  </div>;
+  return (
+    <div>
+      <h1>Open Science</h1>
+      <SpeedInsights />
+    </div>
+  );
 }
 
 export default Home;
